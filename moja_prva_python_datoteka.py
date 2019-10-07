f77b015fcde6324e237b4369f2624d61c53d45b3
Rok Povsic <rok.povsic@gmail.com>
2019-10-07 17:08:33 +0200

    Zacel s projektom

---
+++ moja_prva_python_datoteka.py
@@ -0,0 +1,7 @@
+ime = input("Vpišite svoje ime: ")
+print("Pozdravljen, " + ime + "!")
+
+# To je komentar
+starost = 20  # To je spremenljivka z imenom starost
+print(starost + 10 / 2)
+print("starost")
